5fc29753dfbaeb63b14ed64b44a5d46412f1940b
Rohermy <rohermy.ochoa@gmail.com>
2023-01-15 18:30:58 -0400

    add vercel files

--- src/database/index.js
+++ src/database/index.js
@@ -1,30 +1,13 @@
 import Sequelize from "sequelize";
-//database tables
 
-const sequelize = new Sequelize("systemqr", "postgres", "root", {
-  host: "localhost",
+// const sequelize = new Sequelize("systemqr", "postgres", "root", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
+
+const sequelize = new Sequelize("postgres", "postgres", "Ro33921335*", {
+  host: "db.oriqnytbmffklndrozrr.supabase.co",
   dialect: "postgres",
 });
 
 export default sequelize;
-
-/*mongodb connection*/
-// //import mongoose from "mongoose";
-// //DATABASE CONNECTION
-// // export default function databaseConnetion(app) {
-// //   mongoose
-// //     .connect(
-// //       `mongodb+srv://root:${process.env.MONGO_DB_PASS}@development.gmjmloq.mongodb.net/system-qrstock?retryWrites=true&w=majority`
-// //     )
-// //     .then((result) => {
-// //       console.log(Colors.bgYellow("DATABASE CONNECTION SUCCESS"));
-// //       app.listen(PORT, () => {
-// //         console.log(Colors.dim(`SERVER PORT LISTENING ON PORT ${PORT}`));
-// //       });
-
-// //       // PRODUCT.updateMany({}, { $set: { p_location: "PAPELERIA" } }).then(
-// //       //   (res) => console.log(res)
-// //       // );
-// //     })
-// //     .catch((error) => console.log(Colors.red(error)));
-// // }
