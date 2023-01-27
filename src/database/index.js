9c7bed9e7b6f826ba0ff1f17c8dad6198b4be418
Rohermy <rohermy.ochoa@gmail.com>
2023-01-27 16:15:33 -0400

    add env variables

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,18 @@
 import Sequelize from "sequelize";
 
-// const sequelize = new Sequelize("systemqr", "postgres", "root", {
-//   host: "localhost",
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  }
+);
+
+// const sequelize = new Sequelize("postgres", "postgres", "Ro33921335*", {
+//   host: "db.oriqnytbmffklndrozrr.supabase.co",
 //   dialect: "postgres",
 // });
-
-const sequelize = new Sequelize("postgres", "postgres", "Ro33921335*", {
-  host: "db.oriqnytbmffklndrozrr.supabase.co",
-  dialect: "postgres",
-});
 
 export default sequelize;
